a983480535a976de3d3fac7936a262b2fccc6244
Tomas Enrique <tomasenriquea80@gmail.com>
2018-10-16 23:20:18 +0200

    Initial commit

---
+++ Jugador.java
@@ -0,0 +1,36 @@
+package uF2.ejercicios.inicioPartida;
+
+public class Jugador extends Thread{
+	
+	private int numeroDado;
+	
+	
+	
+	
+	
+	
+	
+	//GETTER
+	public int getNumeroDado() {
+		return numeroDado;
+	}
+
+	//SETTER
+	public void setNumeroDado(int numeroDado) {
+		this.numeroDado = numeroDado;
+	}
+
+
+
+
+
+
+
+
+	public void run() {
+		
+		
+		
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+package uF2.ejercicios.inicioPartida;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		
+		
+
+	}
+
+}
+
+/*
+Queremos hacer un juego multijugador, para decidir quién será el primer jugador en iniciar el turno cada 
+jugador tirará un dado y el que saque mayor puntuación comienza.
+
+Este proceso se puede llevar a cabo simultáneamente (disponemos de un dado por jugador). Cuando todos los 
+jugadores hayan tirado indicaremos cuál es el que empieza tirando.
+
+Si pasado un tiempo prudencial un jugador no ha tirado entenderemos que ha abandonado el juego.
+*/
